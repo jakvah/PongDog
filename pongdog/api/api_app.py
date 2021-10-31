cb69af138dd3159de5f94dccd22aca4210b5c2bc
Jakob Vahlin <jakobvahlin@gmail.com>
2021-10-31 02:11:31 +0200

    Update API endpoints

--- pongdog/api/api_app.py
+++ pongdog/api/api_app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.secret_key = "super secret key"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 """
 General API response codes for DogAPI:
     - 200: Success.
@@ -70,6 +71,84 @@ def test():
         return s
 
 
+@app.route("/get_card_status/<card_id>")
+def get_card_status(card_id):
+    try:
+        if dbm.pong_user_exists(int(card_id)):
+            return "200" # User exists
+        else:
+            return "300" # User not reg
+    except Exception:
+        return "400" # ERROR
+
+@app.route("/get_match_status")
+def get_match_status():
+    try:
+        if dbm.is_match_ongoing():
+            return "200" # ongoing
+        else:
+            return "300" # available match
+    except Exception:
+        return "400" # ERROR
+
+# Start time has format yyyy-mm-ddThh:mm:ss
+# I.e 2021-04-01T07:00:00
+@app.route("/init_match/<p1_id>/<p2_id>/<start_time>",methods = ["GET","POST"])
+def init_match(p1_id,p2_id,start_time):
+    try:
+        if request.method == "GET":
+            return "No access this way!"
+
+        # POST method
+        else:
+            if dbm.is_match_ongoing():
+                return "300" # Ongoing
+            else:
+                if dbm.init_match(p1_id,p2_id,start_time):
+                    return "200"
+                else:
+                    return "400"
+    except Exception:
+        return "400"
+
+@app.route("/reset_match_status/<pwd>")
+def reset_match_status(pwd):
+    if pwd == "pongdg4life":
+        dbm.reset_match_status()
+        return "200"
+    else:
+        return "300"
+
+
+@app.route("/increment_score/<player_num>")
+def increment_score(player_num):
+    if not int(player_num) in [1,2]:
+        return "300" # Invalid player num (not 1 or 2)
+    else:
+        if dbm.increment_score(int(player_num)):
+            return "200" # Success
+        else:
+            return "400"
+
+
+
+@app.route("/get_pongdog_leaderboard",methods = ["GET"])
+def get_pongdog_leaderboard():
+    dataset = dbm.get_sorted_pongdog_leaderboard()
+    scores_list = []
+
+    for i,row in enumerate(dataset):
+        score_dict = {}
+
+        score_dict["name"] = row[2]
+        score_dict["id"] = row[1]
+        score_dict["score"] = row[3]
+        score_dict["rank"] = i+1
+        scores_list.append(score_dict)
+    obj = {"scores" : scores_list}
+    response = jsonify(obj)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 
 
@@ -80,4 +159,5 @@ def reverseBytes(number):
     byteList = [binary[i:i+8][::-1] for i in range(0, 32, 8)] # Reverse each byte
     return int(''.join(byteList), 2) # Join and convert to decimal
     # return int(''.join(["{0:0>32b}".format(number)[i:i+8][::-1] for i in range(0, 32, 8)]), 2)
+
 
--- pongdog/api/db_manager.py
+++ pongdog/api/db_manager.py
@@ -16,12 +16,11 @@ def get_database_connection():
         #print("Could not establish connection to the database. Is the server running?")
         return f"Failed, {e}"
 
-
 def add_pong_dog_user(card_id,user_name):
     conn = get_database_connection()
     cur = conn.cursor()
-    query = "INSERT INTO pongdog_users (cardid,name,elo,games_played) values (%s,%s,%s,%s)"
-    cur.execute(query,(int(card_id),str(user_name),1000,0))
+    query = "INSERT INTO pongdog_users (cardid,name,elo,games_played,wins) values (%s,%s,%s,%s,%s)"
+    cur.execute(query,(int(card_id),str(user_name),1000,0,0))
     conn.commit()
     cur.close()
     conn.close()
@@ -43,21 +42,80 @@ def pong_user_exists(id,table_name = "pongdog_users"):
         return False
 
 
-
-
-# --------------------------------- PONG DOG FUNCTIONS --------------------------------- #
-
-def add_pong_dog_user(card_id,user_name):
+def is_match_ongoing():
     conn = get_database_connection()
     cur = conn.cursor()
-    query = "INSERT INTO pongdog_users (cardid,name,elo,games_played) values (%s,%s,%s,%s)"
-    cur.execute(query,(int(card_id),str(user_name),1000,0))
+    query = "SELECT * FROM active_game"
+    cur.execute(query)
+    set = cur.fetchall()[0]
+    if int(set[0]) == 1:
+        return True
+    else:
+        return False
+
+
+def reset_match_status():
+    conn = get_database_connection()
+    cur = conn.cursor()
+    query = "delete from active_game"
+    cur.execute(query)
+    conn.commit()
+
+    query = "INSERT INTO active_game (ongoing,player1_id,player2_id,player1_score,player2_score,start_time) values(%s,%s,%s,%s,%s,%s)"
+
+    cur.execute(query,(0,-1,-1,-1,-1,"-"))
     conn.commit()
     cur.close()
     conn.close()
 
 
+def init_match(p1_id,p2_id,start_time):
+    try:
+        conn = get_database_connection()
+        cur = conn.cursor()
+        query = "delete from active_game"
+        cur.execute(query)
+        conn.commit()
 
+        query = "INSERT INTO active_game (ongoing,player1_id,player2_id,player1_score,player2_score,start_time) values(%s,%s,%s,%s,%s,%s)"
+
+        cur.execute(query,(1,int(p1_id),int(p2_id),0,0,str(start_time)))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except Exception:
+        return False
+
+def increment_score(player_num):
+    try:
+        conn = get_database_connection()
+        cur = conn.cursor()
+
+        if player_num == 1:
+            query = "UPDATE active_game SET player1_score = player1_score + 1 WHERE ongoing = 1"
+        elif player_num == 2:
+            query = "UPDATE active_game SET player2_score = player2_score + 1 WHERE ongoing = 1"
+
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except Exception:
+        return False
+
+
+def get_sorted_pongdog_leaderboard():
+    conn = get_database_connection()
+    cur = conn.cursor()
+    query = "SELECT * FROM pongdog_users ORDER BY elo DESC"
+
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
 
 
 
