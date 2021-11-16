6aaac49ed4864adb6dea0de24690438795e07945
leiklier <leik.lima-eriksen@lyse.net>
2021-11-16 18:24:10 +0100

    LogDog: Trafikklys

--- pongdog-web/src/index.tsx
+++ pongdog-web/src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import TimeAgo from "javascript-time-ago";
+
+import en from "javascript-time-ago/locale/en.json";
+TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- pongdog-web/src/pages/Logdog.tsx
+++ pongdog-web/src/pages/Logdog.tsx
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
 import { BsCircleFill } from "react-icons/bs";
+import ReactTimeAgo from "react-time-ago";
 import { useInterval } from "../hooks";
 
 const SECONDS = 1000;
 const MINUTES = 60 * SECONDS;
 
-const RoomCard: React.FC<{ id: number; name: string }> = ({ id, name }) => {
-  const [users, setUsers] = useState<Array<{ id: number; name: string }>>([]);
+interface User {
+  id: number;
+  name: string;
+  lastSeenAt: Date;
+}
+
+const UserActivity: React.FC<{ user: User }> = ({ user }) => {
+  const [color, setColor] = useState<"green" | "orange" | "red">("green");
+  useInterval(() => {
+    const diffMs = new Date().getTime() - new Date(user.lastSeenAt).getTime();
+    const diffMins = Math.floor(((diffMs % 86400000) % 3600000) / 60000);
+    if (diffMins < 5) {
+      setColor("green");
+    } else if (diffMins < 15) {
+      setColor("orange");
+    } else {
+      setColor("red");
+    }
+  }, 5 * SECONDS);
+  return (
+    <div className="room-card__content__user" key={user.id}>
+      <BsCircleFill style={{ color }} />
+      {user.name}
+      <div className="room-card__content__user__activity">
+        (<ReactTimeAgo date={user.lastSeenAt} />)
+      </div>
+    </div>
+  );
+};
+
+const RoomCard: React.FC<{
+  id: number;
+  name: string;
+}> = ({ id, name }) => {
+  const [users, setUsers] = useState<Array<User>>([]);
   useInterval(async () => {
     const response = await fetch(
       `https://jakvah.pythonanywhere.com/logdog/rooms/${id}`
     ).then((response) => response.json());
     setUsers(response.data.users);
-  }, 10 * SECONDS);
+  }, 2 * MINUTES);
+
   return (
     <div className="room-card">
       <div className="room-card__header">{name}</div>
       <div className="room-card__content">
         {users.map((user) => (
-          <div className="room-card__content__user" key={user.id}>
-            <BsCircleFill style={{ color: "green" }} />
-            {user.name}
-            <div className="room-card__content__user__activity">
-              (Last seen: 2h ago)
-            </div>
-          </div>
+          <UserActivity key={user.id} user={user} />
         ))}
       </div>
     </div>
