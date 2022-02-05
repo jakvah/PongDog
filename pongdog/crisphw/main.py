c749d8f44faa08471104837a6ec90023fd426320
Matias <matias_haugum@hotmail.com>
2022-02-05 15:09:11 +0100

    player class

--- pongdog/crisphw/main.py
+++ pongdog/crisphw/main.py
@@ -5,12 +5,13 @@ import time
 
 sound.play_nice()
 time.sleep(2)
-while(True):
-    sound.play_score_sound()
-    time.sleep(0.5)
-    #Cardreader.run()
 
+class Player:
+    def __init__(self,card_id, score):
+        self.card_id = card_id
+        self.score = score
 
+    
 #1. Poll Cards
 #2. Start game
 #3. Post score
