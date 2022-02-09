6f99c08426f1576e328320327b6907076b8991f3
Matias Haugum <matias_haugum@hotmail.com>
2022-02-09 19:39:30 +0100

    now seeds from main

--- pongdog/crisphw/main.py
+++ pongdog/crisphw/main.py
@@ -1,15 +1,12 @@
 from multiprocessing import Condition
 from utils import sound
 from utils.cardreader import Cardreader
-import time
+import time, random
 
+random.seed()
 sound.play_nice()
 time.sleep(2)
 
-class Player:
-    def __init__(self,card_id, score):
-        self.card_id = card_id
-        self.score = score
 
     
 #1. Poll Cards
