fe7aeaf2efe1ff6a916d8f05a93b23c5be278182
Matias Haugum <matias_haugum@hotmail.com>
2022-02-16 17:49:03 +0100

    implementing graphics

--- pongdog/crisphw/main.py
+++ pongdog/crisphw/main.py
@@ -1,14 +1,11 @@
 from multiprocessing import Condition
-from utils import sound
+from utils import sound, game
 from utils.cardreader import Cardreader
 import time, random
 
 random.seed()
-sound.play_nice()
-time.sleep(2)
 
-
-    
+game.start_game(100,102)
 #1. Poll Cards
 #2. Start game
 #3. Post score
--- pongdog/crisphw/utils/game.py
+++ pongdog/crisphw/utils/game.py
@@ -1,4 +1,5 @@
-import math, time, random
+import math, time, random, pygame
+from utils import sound
 
 GAME_TIMEOUT = 600 #Game automatically stops after 600 seconds // 10 minutes
 
@@ -17,10 +18,27 @@ class Player:
 
 # main game logic
 def start_game(p1, p2):
-
     start_time = time.time()
     player1, player2 = Player(p1), Player(p2)
     
+    # ------- Window settings
+    pygame.init()
+    flags = pygame.FULLSCREEN
+    screen = pygame.display.set_mode((1920,1080),flags)
+    pygame.display.set_caption("PongDog")
+
+    # ------- Text
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    # ------- Game functions
+    def show_score(x,y,score):
+        score = font.render("Score: " + str(score), True, (0,0,0))
+        screen.blit(score, (x, y))
+
+    def draw_circle(x,y):
+        circle = pygame.draw.circle(screen, (126,126,126),(x,y),200)
+
+    # ------ Serve
     if random.randint(0,1) == 1:
         print("P1 serves")
         player1.server = True
@@ -29,6 +47,9 @@ def start_game(p1, p2):
         player2.server = True
     
     while True: #main game loop
+        screen.fill((255,255,255))
+
+
         current_time = time.time()
         delta_time = round((current_time - start_time))
         if abs(player1.score-player2.score) >= 2 and (player1.score >= 11 or player2.score >= 11): # Game is won by normal means
@@ -70,12 +91,47 @@ def start_game(p1, p2):
         if random.randint(0,1) == 1:
             print("P1 scores!")
             player1.increment_score()
+            sound.play_score_sound()
+            
         else:
             print("P2 scores!")
             player2.increment_score()
+            sound.play_score_sound()
 
         print("p1 score:" +  str(player1.score))
         print("p2 score:" +  str(player2.score))
         print("------")
+        # ------- Update score, draw objects
+        show_score(0,50,player1.score)
+        show_score(650,50,player2.score)
+        draw_circle(500,500)
+        pygame.display.update()
+        time.sleep(1)
+
+
+def pygame_test():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("PongDog")
+    font = pygame.font.Font('freesansbold.ttf', 64)
+
+    def show_score(x,y):
+        score = font.render("Score: " + str(20), True, (0,0,0))
+        screen.blit(score, (x, y))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((255,255,255))
+        show_score(10,10)
+        show_score(100,200)
+
+        pygame.display.update()
+
+
 if __name__ == "__main__":
     start_game(69,20)
+    #pygame_test()
