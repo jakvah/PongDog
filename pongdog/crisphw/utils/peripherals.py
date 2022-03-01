381fbed24f7f456ce2f6a3d72e60cacd68cd23e4
Matias Haugum <matias_haugum@hotmail.com>
2022-03-01 22:28:58 +0100

    can now differentiate between png and jpg

--- pongdog/crisphw/utils/peripherals.py
+++ pongdog/crisphw/utils/peripherals.py
@@ -1,13 +1,28 @@
-import requests, os
+import requests, os, wget, imghdr
 
 def fetch_player_image(playerID):
     if os.path.isfile("images/profilepictures/"+str(playerID)+".png") or os.path.isfile("images/profilepictures/"+str(playerID)+".jpg"):
         print("Profile picture already exists.")
-        return
+        #return
     response = requests.get("https://jakvah.pythonanywhere.com/static/imgs/"+str(playerID))
-    file = open("images/profilepictures/"+str(playerID)+".png", "wb")
-    file.write(response.content)
-    file.close()
+    print(response.status_code)
+    if response.status_code != 200:
+        print("Profile picture does not exist on server")
+        #return
+    
+    print(imghdr.what("oi", h=response.content))
+    #^bruk den her til å finne ut om du skal lagre som png eller jpg!
+
+    filejpg = open("images/profilepictures/"+str(playerID)+".jpg", "wb")
+    filejpg.write(response.content)
+    filejpg.close()
+    # - png 
+    filepng = open("images/profilepictures/"+str(playerID)+".png", "wb")
+    filepng.write(response.content)
+    filepng.close()
+    print(imghdr.what("images/profilepictures/317094323.jpg",h=response.content))
+
 
 if __name__ == "__main__":
     fetch_player_image(2)
+
